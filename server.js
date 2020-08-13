3eee77a2dadf4b6226d0da419958e1f176ef0fd1
Henry <henrysavit@gmail.com>
2020-08-13 14:29:44 -0400

    update server so that runs in node env in tests

--- server.js
+++ server.js
@@ -1,40 +1,49 @@
+// import {handlers} from "./handlers"
 import {graphql} from 'msw';
 import {setupServer} from 'msw/native';
 
-export default setupServer(
-    graphql.query('getCharacters', (req, res, ctx) => {
-      return res(
-        ctx.data({
-          characters: {
-            results: [
-              {
-                __typename: 'Character',
-                id: '1',
-                name: 'Guy',
-              },
-              {
-                __typename: 'Character',
-                id: '2',
-                name: 'Girl',
-              },
-              {
-                __typename: 'Character',
-                id: '3',
-                name: 'Joe',
-              },
-              {
-                __typename: 'Character',
-                id: '4',
-                name: 'Jane',
-              },
-              {
-                __typename: 'Character',
-                id: '5',
-                name: 'John Smith',
-              },
-            ],
+let mswSetupServer = setupServer;
+
+if (process.env.NODE_ENV === 'test') {
+  let {setupServer} = require('msw/node');
+  mswSetupServer = setupServer;
+}
+
+const query = graphql.query('getCharacters', (req, res, ctx) => {
+  return res(
+    ctx.data({
+      characters: {
+        results: [
+          {
+            __typename: 'Character',
+            id: '1',
+            name: 'Guy',
           },
-        }),
-      );
+          {
+            __typename: 'Character',
+            id: '2',
+            name: 'Girl',
+          },
+          {
+            __typename: 'Character',
+            id: '3',
+            name: 'Joe',
+          },
+          {
+            __typename: 'Character',
+            id: '4',
+            name: 'Jane',
+          },
+          {
+            __typename: 'Character',
+            id: '5',
+            name: 'John Smith',
+          },
+        ],
+      },
     }),
   );
+});
+
+const server = mswSetupServer(query);
+export default server;
